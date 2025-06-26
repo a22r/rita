8bf53464b6708653b5fb736b682aa7a306937205
a22r <francehhh@outlook.com>
2025-06-26 22:48:10 +0100

    update

--- src/main/java/com/rita/config/WebConfig.java
+++ src/main/java/com/rita/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .maxAge(3600);
     }
 } 
